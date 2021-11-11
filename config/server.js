f448185e956bce28b6b948a5491b4fc4c6b5ee4b
achingachris <achinga.chris@gmail.com>
2021-11-11 15:52:52 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c01100d113e9421d8d9de70f035d4ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '98dcadee-0ced-4ef9-8ee4-3a8f6aec88b3'
+};
