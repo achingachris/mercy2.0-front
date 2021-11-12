c1bb39629556e0916dbe406b069eea87c82f88a5
achingachris <achinga.chris@gmail.com>
2021-11-12 11:12:13 +0300

    db and cloudinary config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0c01100d113e9421d8d9de70f035d4ab'),
+      secret: env("ADMIN_JWT_SECRET", "0c01100d113e9421d8d9de70f035d4ab"),
     },
   },
+});
+
+module.exports = ({ env }) => ({
+  url: env("MY_HEROKU_URL"),
 });
