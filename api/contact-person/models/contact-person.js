ba3341fcb430742494eaae95ae35457221ca9ae4
achingachris <achinga.chris@gmail.com>
2021-11-20 13:10:39 +0300

    update docs

--- api/contact-person/models/contact-person.js
+++ api/contact-person/models/contact-person.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require("slugify");
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.name) {
+        data.slug = slugify(data.name);
+      }
+    },
+  },
+};
+
+// module.exports = {};
