67708f07fafbc8ba5a326362b94fb3b9021b3191
chrisdevcode <achinga.chris@gmail.com>
2022-06-21 12:02:50 +0300

    clean

--- admin/src/config.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_LOGO = "./assets/images/logo-strapi.png";
-export const SHOW_TUTORIALS = false;
-export const SETTINGS_BASE_URL = '/settings';
-export const STRAPI_UPDATE_NOTIF = false;
